c85dec424dbb43d7501bca9955dfd13c2b4bc68c
NewDoer <lion@crazymcn.com>
2025-11-28 22:08:08 +0800

    updata placeholder

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,4 +1,6 @@
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter.
+// Updated export format
+
 export const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
